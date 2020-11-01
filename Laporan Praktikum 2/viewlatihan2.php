e90ea341f232ed905343c5b3eae4b6927878e066
Muhamad Nur Alim <56181126+NurAlim05@users.noreply.github.com>
2020-11-01 14:12:28 +0700

    Add files via upload

---
+++ Laporan Praktikum 2/viewlatihan2.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html>
+<head>
+	<title>Latihan 2</title>
+</head>
+<body>
+
+	Hello friends,let's learn web programming ..<br>
+	Nilai 1 = <?= $nilai1; ?>
+	Nilai 2 = <?= $nilai2; ?>
+	ini hasil dari permodelan dengan metide penjumlahan
+	<?= $nilai1."+".$nilai2."=".$hasil; ?>
+</body>
+</html> 
