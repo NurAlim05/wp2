834600b3af87e46a1b3e69fb1c5133a29df42bd9
Muhamad Nur Alim <56181126+NurAlim05@users.noreply.github.com>
2020-11-01 14:10:29 +0700

    Add files via upload

---
+++ Laporan Praktikum 1/perkenalan.php
@@ -0,0 +1,15 @@
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class perkenalan extends CI_Controller{
+
+        public function index(){
+            echo "<h1 style='color:black; text-align:center'>Perkenalan</h1>";
+            echo "<p>Nama Saya Muhamad Nur Alim <br>
+                    Saya Tinggal Di Tegal <br> 
+                    ";
+        }
+
+    }
+
+?>
